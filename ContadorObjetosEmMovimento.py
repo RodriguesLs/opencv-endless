f9876a6854cb8eadd36a6e46723b0406f69b121e
Rodrigues-lenovo <rodrigues@devires.com.br>
2019-11-02 20:30:29 -0300

    lies

--- ContadorObjetosEmMovimento.py
+++ ContadorObjetosEmMovimento.py
@@ -107,10 +107,10 @@ while True:
     
     #testa interseccao dos centros dos contornos com as linhas de referencia
     #dessa forma, contabiliza-se quais contornos cruzaram quais linhas (num determinado sentido)
-	if (TestaInterseccaoEntrada(CoordenadaYCentroContorno,CoordenadaYLinhaEntrada,CoordenadaYLinhaSaida)):
-    ContadorEntradas += 1
-	if (TestaInterseccaoSaida(CoordenadaYCentroContorno,CoordenadaYLinhaEntrada,CoordenadaYLinhaSaida)):  
-    ContadorSaidas += 1
+	  if (TestaInterseccaoEntrada(CoordenadaYCentroContorno,CoordenadaYLinhaEntrada,CoordenadaYLinhaSaida)):
+      ContadorEntradas += 1
+	  if (TestaInterseccaoSaida(CoordenadaYCentroContorno,CoordenadaYLinhaEntrada,CoordenadaYLinhaSaida)):  
+      ContadorSaidas += 1
 
   #Se necessario, descomentar as lihas abaixo para mostrar os frames utilizados no processamento da imagem
   #cv2.imshow("Frame binarizado", FrameThresh)
